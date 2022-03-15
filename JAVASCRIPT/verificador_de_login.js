59accd36e5d090aec3f2954bb6c8e65865bed4a0
MatheusFelipeRM <98364495+MatheusFelipeRM@users.noreply.github.com>
2022-03-15 07:30:12 -0300

    n

--- JAVASCRIPT/verificador_de_login.js
+++ JAVASCRIPT/verificador_de_login.js
@@ -52,3 +52,40 @@ var quantidade_recebido = document.getElementById("quantidade").value
 
 }
 
+function remove_no_carrinho(produtos){
+    var tabela = document.getElementById("tabela")
+    nova_linha= "<tr><td>"+produtos['serial']+"</td> <td>"+produtos["nome"]+"</td> <td>"+produtos["preco"]+" R$"+"</td> <td>"+"removido"+"</td>"
+    tabela.innerHTML += nova_linha;
+}
+function remover(){
+    var serial_recebido = document.getElementById("caixa_de_texto").value
+    var div = document.getElementById('center')
+
+    for(var i = 0; i < produtos.length; i++){
+        var auxiliar =   produtos[i]
+
+        if(auxiliar["serial"]==serial_recebido){
+           var resposta = Number(prompt('Digite 1 para sim e 2 para não'))
+           remove_no_carrinho(auxiliar);
+           if(resposta==1){
+            auxiliar.splice(0, 1);
+            
+             
+           }else if(resposta==2){
+               alert("não excluido")
+           }
+
+          
+           
+            
+            
+
+        
+        }
+
+       
+
+}
+alert("esse "+auxiliar['nome'])
+}
+
